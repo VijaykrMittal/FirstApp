d4df351d2700aa419df7bff7ce5914f927438510
Ramit Arora <ravic@biz2credit.com>
2014-11-18 13:12:01 +0000

    Changes made by Ramit Arora (ravic@biz2credit.com).

--- Biz2CreditApp/scripts/documentPending.js
+++ Biz2CreditApp/scripts/documentPending.js
@@ -20,6 +20,45 @@
             $(".documentList .wrap-content .row .name a").on("click.myPlugin", function() {
                 $(this).parents('.row').next(".filesBlock").slideToggle();	
             });
+            
+            
+            var dataSource = new kendo.data.DataSource({
+                    transport: {
+                        
+                        read: {
+                            url: localStorage.getItem("urlMobAppApiLoan"),
+                            type:"POST",
+                            dataType: "json", // "jsonp" is required for cross-domain requests; use "json" for same-domain requests
+                            data: { apiaction:'reqdocuments',matchid:82945,cust_id:94351,appid:70397} // search for tweets that contain "html5"
+                        }
+                        
+                    },
+                    schema: {
+                        data: function(data)
+                        {
+                            console.log(data);
+                        	return [data];
+                        }
+                    },
+                    error: function (e) {
+                    	apps.hideLoading();
+                    	navigator.notification.alert("Server not responding properly.Please check your internet connection.",
+                    	function () { }, "Notification", 'OK');
+                    },
+                });
+                dataSource.fetch(function(){
+                    var that = this;
+                    var data = that.view(); 
+                    console.log(data);
+                    apps.hideLoading();
+                    if(data[0]['results']['faultcode']===1 && data[0]['results']['faultmsg']==='success')
+                    {
+                         app.documentService.viewModel.loadRequirementDocs(data['0']['results']['ReqDocs']);
+                         
+                    }
+                });
+            
+            
         },
         UploadExisting:function()
         {
@@ -61,7 +100,20 @@
                          apps.navigate("#views/matches.html");
                     }
                 });
+        },
+        loadRequirementDocs:function(data)
+        {
+            var template = kendo.template($("#requiredocs-template").html());
+            console.log(data);
+            console.log(template);
+           
+            var result = template(data); //Execute the template
+             console.log(result);
+             $("#requireDocsList").html(result);
+           // kendo.bind($("#requireDocsList"), app.documentService.viewModel);
+            
         }
+        
     });
     app.documentService ={
         viewModel:new DocumentPendingModel()

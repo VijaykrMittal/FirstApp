7a147c534aa767bdc3a7f3f661aba69732884906
Ramit Arora <ravic@biz2credit.com>
2014-11-20 10:48:45 +0000

    Changes made by Ramit Arora (ravic@biz2credit.com).

--- Biz2CreditApp/scripts/documentPending.js
+++ Biz2CreditApp/scripts/documentPending.js
@@ -29,7 +29,8 @@
                             url: localStorage.getItem("urlMobAppApiLoan"),
                             type:"POST",
                             dataType: "json", // "jsonp" is required for cross-domain requests; use "json" for same-domain requests
-                            data: { apiaction:'reqdocuments',matchid:matchid,cust_id:localStorage.getItem("userID"),appid:appid} // search for tweets that contain "html5"
+                           data: { apiaction:'reqdocuments',matchid:matchid,cust_id:localStorage.getItem("userID"),appid:appid}
+                          //  data: { apiaction:'reqdocuments',matchid:82795,cust_id:localStorage.getItem("userID"),appid:70386}// search for tweets that contain "html5"
                         }
                         
                     },
@@ -55,7 +56,7 @@
                          
                     }
                 });
-            
+           
             
         },
         UploadExisting:function()
@@ -101,12 +102,40 @@
         },
         loadRequirementDocs:function(data)
         {
-            var template = kendo.template($("#requiredocs-template").html());
-            var result = template(data); //Execute the template
-            $("#requireDocsList").html(result);
+            html = '';
+            for (var i = 0; i < data.length; i++) { 
+                html+= '<div class="row clearfix" ><div class="num"><span>'+(i+1)+'</span></div>';
+                html+= '<div class="name requiredocs-name">';
+                if(data[i].DocFileDetails===false) { 
+                    html+=  data[i].docName;
+                }else{
+                    html+= '<a id="dispalyFiles">'+data[i].docName+'</a><span>('+data[i].DocFileDetails.length+')</span>';
+                }           
+                if(data[i].b2cdocid === "148" || data[i].b2cdocid === 148) {
+                    html+=        '<div class="text1"><span class="text2"><a href="javascript:void(0);" id="esignAnchor" onclick="sendEmailToUsr()">Resend esign document</a></span></div><div><span class="docuploaded"><a href="https://www.biz2beta.com/components/com_financialnew/create_term.php?appid=72115&amp;lenderorg_id=334">Download document for wet signature</a></span></div>';
+                }
+                html+= '</div>';
+                html+= '<div class="upload"><a data-bind="click:UploadExisting" class="active appDocFile">Upload New/Select Existing</a></div>';
+                html+='</div>';
+                html+= '<div class="filesBlock">';
+                if(data[i].DocFileDetails !==false) { 
+                    fileData =data[i].DocFileDetails;
+                    html+='<div class="sub-row clearfix" id="attachFilesList-'+data[i].id+'" >'; 
+                    for (var j = 0; j < fileData.length; j++) {
+                        html+='<div class="file-iconjpg"></div><div class="file-name">'+fileData[j].name+'</div><div class="buttonSet"><a href="https://www.biz2beta.com/bizdownload.php?fid=82edc5c9e21035674d481640448049f3" title="Download" class="downloadBtn" target="_blank">Download</a><a href="javascript:void(0);" onclick="getDeleteMatchDocFile(22436,4226,94351,82945);" title="Delete" class="DeleteIC">Delete</a></div>';
+                    }
+                    html+='</div>';
+                }
+                html+='</div>';
+            }
+            $("#requireDocsList").html(html);
             kendo.bind($("#requireDocsList"), app.documentService.viewModel);
+
+            $('.requiredocs-name').click(function() {
+                $(this).parent().next().slideToggle('fast');
+            });
             
-        }
+        },
         
     });
     app.documentService ={

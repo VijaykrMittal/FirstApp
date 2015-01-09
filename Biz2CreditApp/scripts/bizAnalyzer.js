1ffceffc84162172be6236195ae049e13cb4c8ba
Ramit Arora <ravic@biz2credit.com>
2015-01-09 08:46:15 +0000

    Changes made by Ramit Arora (ravic@biz2credit.com).

--- Biz2CreditApp/scripts/bizAnalyzer.js
+++ Biz2CreditApp/scripts/bizAnalyzer.js
@@ -6,13 +6,13 @@
         showrefreshBiz:true,
         showAfter:function()
         {
-            var pcsVal =	600;
+            var pcsVal =	512;
             var dtiVal =	100;
-            var tibVal =	30;
-            var cfVal  =	48;
+            var tibVal =	55;
+            var cfVal  =	-30;
             var irVal  =	1;
-            var crVal  =	0;
-            var arVal  =	2900000;
+            var crVal  =	1;
+            var arVal  =	'';
             console.log('call');
            jQuery("#personalCreditSliderTxt").val(pcsVal);
         	jQuery("#debitToIncomeSliderTxt").val(dtiVal);
@@ -45,13 +45,13 @@
             
             
 
-            var pcsVal =	600;
+            var pcsVal =	512;
             var dtiVal =	100;
-            var tibVal =	30;
-            var cfVal  =	48;
+            var tibVal =	55;
+            var cfVal  =	-30;
             var irVal  =	1;
-            var crVal  =	0;
-            var arVal  =	2900000;
+            var crVal  =	1;
+            var arVal  =	'';
 
           /*   var pcsVal =	parseInt(document.getElementById('personalCreditSliderTxt').value);
             var dtiVal =	Math.round(parseInt(document.getElementById('debitToIncomeSliderTxt').value));

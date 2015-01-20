8d537bd5b338f649833c138065cc6bd45dcfe02a
Ramit Arora <ravic@biz2credit.com>
2015-01-20 08:40:07 +0000

    Changes made by Ramit Arora (ravic@biz2credit.com).

--- Biz2CreditApp/scripts/bizAnalyzer.js
+++ Biz2CreditApp/scripts/bizAnalyzer.js
@@ -1,10 +1,28 @@
-(function (global) {
-    var bizAnalyzerViewModal,
-        app = global.app = global.app || {};
+var app = app || {};
 
-    bizAnalyzerViewModal = kendo.data.ObservableObject.extend({
-        showrefreshBiz:true,
-        showAfter:function()
+app.bizAnalyzer = (function () {
+    'use strict'
+    
+     var bizAnalyzerViewModal = (function () {
+         
+        var showrefreshBiz=kendo.observable({ 
+            showrefreshBizVal: true,
+                
+            setShowrefreshBizFalse:function()
+            { 
+                console.log(kendo);
+                var that = this;
+                that.set("showrefreshBizVal",false);
+
+            },
+            setShowrefreshBizTrue:function()
+            {
+                var that = this;
+                that.set("showrefreshBizVal",true);
+            },
+        });
+    
+        var showAfter=function()
         {
             var pcsVal =	512;
             var dtiVal =	100;
@@ -40,9 +58,9 @@
             console.log('calanurevFlow'+calanurevFlow);
              */
 
-            app.bizAnalyzer.viewModel.getTotal();
-        },
-        show:function()
+           getTotal();
+        };
+        var show=function()
         {
             jQuery.browser = {};
             (function () {
@@ -89,14 +107,14 @@
 
             $(window).load(function(){
 
-                slideTimer = setInterval(function() {
+               var slideTimer = setInterval(function() {
                 	$('.bz_an_mn_pg_inr_lw_tbs_cton').fadeOut();
                 }, 4000);
             });
 
             var tooltipElement = '<div class="ui-slider-tooltip" aria-hidden="true" />';
             var tooltip;
-            url = ("https:" === document.location.protocol ? "https://" : "http://") + "www.biz2beta.com";
+            var url = ("https:" === document.location.protocol ? "https://" : "http://") + "www.biz2beta.com";
 
             $('#personalCreditSlider').slider({
                 range: "min",     
@@ -118,12 +136,12 @@
                     $('#personalCreditSlider .ui-slider-tooltip').text( ui.value );
                     $('#personalCreditSlider .ui-slider-tooltip').attr('aria-hidden', 'false');
                     $( "#personalCreditSliderTxt" ).val(ui.value);
-                    app.bizAnalyzer.viewModel.getTotal();
+                    getTotal();
                 },
                 slide: function ( event, ui ) {
                     
                     $( "#personalCreditSliderTxt" ).val(ui.value);
-                    app.bizAnalyzer.viewModel.getTotal();
+                    getTotal();
                     $('.ui-slider-tooltip').attr('aria-hidden', 'true');
                 }
             });
@@ -149,12 +167,12 @@
                     $('#debitToIncomeSlider .ui-slider-tooltip').text( ui.value );
                     $('#debitToIncomeSlider .ui-slider-tooltip').attr('aria-hidden', 'false');
                     $( "#debitToIncomeSliderTxt" ).val(ui.value);
-                    app.bizAnalyzer.viewModel.getTotal();
+                    getTotal();
                 },
                 slide: function ( event, ui ) {
                     
                     $( "#debitToIncomeSliderTxt" ).val(ui.value);
-                    app.bizAnalyzer.viewModel.getTotal();
+                    getTotal();
                     $('.ui-slider-tooltip').attr('aria-hidden', 'true');
 
                 }
@@ -181,14 +199,14 @@
                     $('#timeInBussinessSlider .ui-slider-tooltip').text( ui.value );
                     $('#timeInBussinessSlider .ui-slider-tooltip').attr('aria-hidden', 'false');
                     $( "#timeInBussinessSliderTxt" ).val(ui.value);
-                    app.bizAnalyzer.viewModel.getTotal();
+                    getTotal();
 
 
                 },
                 slide: function ( event, ui ) {
 
                     $( "#timeInBussinessSliderTxt" ).val(ui.value);
-                    app.bizAnalyzer.viewModel.getTotal();
+                    getTotal();
                     $('.ui-slider-tooltip').attr('aria-hidden', 'true');
 
                 }
@@ -220,7 +238,7 @@
                         $( "#industryRiskSliderTxt1" ).val("Low Risk");
                     }
 
-                    app.bizAnalyzer.viewModel.getTotal();
+                    getTotal();
                     $('.ui-slider-tooltip').attr('aria-hidden', 'true');
 
                 }
@@ -249,7 +267,7 @@
                     if(ui.value>2 ){
                        $( "#corporateRiskSliderTxt1" ).val("High Risk");
                     }
-                    app.bizAnalyzer.viewModel.getTotal();
+                    getTotal();
                     $('.ui-slider-tooltip').attr('aria-hidden', 'true');
 
                 }
@@ -275,14 +293,14 @@
                     $('#cashFlowSlider .ui-slider-tooltip').text( ui.value );
                     $('#cashFlowSlider .ui-slider-tooltip').attr('aria-hidden', 'false');
                     $( "#cashFlowSliderTxt" ).val(ui.value);
-                    app.bizAnalyzer.viewModel.getTotal();
+                    getTotal();
 
 
                 },
                 slide: function ( event, ui ) {
                     
                     $( "#cashFlowSliderTxt" ).val(ui.value);
-                    app.bizAnalyzer.viewModel.getTotal();
+                    getTotal();
                     $('.ui-slider-tooltip').attr('aria-hidden', 'true');
 
                 }
@@ -319,7 +337,7 @@
                         $( "#anuRevSliderTxt1" ).val("Low Risk");
                     }
                     $( "#anuRevSliderTxt" ).val(ui.value);
-                    app.bizAnalyzer.viewModel.getTotal();
+                    getTotal();
 
                 },
                 slide: function ( event, ui ) {
@@ -335,14 +353,14 @@
                     }
                     $( "#anuRevSliderTxt" ).val(ui.value);
 
-                    app.bizAnalyzer.viewModel.getTotal();
+                    getTotal();
                     $('.ui-slider-tooltip').attr('aria-hidden', 'true');
 
                 }
             });
-            app.bizAnalyzer.viewModel.setShowrefreshBizTrue(); 
-        },
-        getTotal:function(){
+            showrefreshBiz.setShowrefreshBizTrue(); 
+        };
+        var getTotal=function(){
             
             var url = ("https:" === document.location.protocol ? "https://" : "http://") + "www.biz2beta.com";
             var calcScore = jQuery("#personalCreditSliderTxt").val()
@@ -354,84 +372,39 @@
             var calanurevFlow = Math.round(jQuery("#anuRevSliderTxt").val())
 
             var newurl=url+"/components/com_bizanalyzertool/sliderCalculation.php";
-            $.post(newurl,{creditScore:calcScore,dtiRatio:calcDebt,ageOfBiss:calcTimeBusin,industxt:calcIndusRisk,corpTxt:calcCorpRisk,cashMargin:calcCashFlow,anuRevenue:calanurevFlow},app.bizAnalyzer.viewModel.getSuccessData1,"json");	
-        },
+            $.post(newurl,{creditScore:calcScore,dtiRatio:calcDebt,ageOfBiss:calcTimeBusin,industxt:calcIndusRisk,corpTxt:calcCorpRisk,cashMargin:calcCashFlow,anuRevenue:calanurevFlow},getSuccessData1,"json");	
+        };
 	
-    	getSuccessData1:function(data){ 
+    	var getSuccessData1=function(data){ 
     		jQuery('#netScoreBizanalyserTxt').text(data.total);
-    	},
-        /*refres:function(myvalue)
+    	};
+        
+        
+        var refreshViewBiz=function()
         {
-        	var url = ("https:" === document.location.protocol ? "https://" : "http://") + "www.biz2beta.com";
-        	var slider  = $('.slider > div').slider({
-        	range: "min",     
-        	value: myvalue,
-        	min: 500,
-        	max: 850,
-        	step: 1,
-        	create: function ( event ) {
-        		$( event.target ).find('.ui-slider-handle').append( tooltipElement );
-        		tooltip = $('.ui-slider-tooltip');
-        	},
-            change: function ( event, ui ) {
-            	var input = $( event.target ).parent().find('input.answer');
-            	input.val( ui.value );
-            	var tooltipvalue=ui.value;
-            	window.tooltip.text( ui.value );
-            	window.tooltip.attr('aria-hidden', 'false');
-            	alert(url);
-            	$.ajax({
-            		url:url+'/components/com_nomatch/action.php?tooltipval='+tooltipvalue+'&div=1',
-            		type: "get",
-            		success:function(data) { 
-             	 	 $('#bz_an_mn_tbs_cont1').html(data); 
-            	    }    
-                });
-                $.ajax({
-                  
-                   url:url+'/components/com_nomatch/action.php?tooltipval='+tooltipvalue+'&div=2',
-                    type: "get",
-                    success:function(data) { 
-                      $('#bz_an_mn_tbs_cont2').html(data); 
-                    }   
-
-                });
-
-
-              },
-            slide: function ( event, ui ) {
-                window.tooltip.attr('aria-hidden', 'true');
-            	}
-            });
-        },*/
-        setShowrefreshBizFalse:function()
-        {
-            var that = this;
-            that.set("showrefreshBiz",false);
-        },
-        setShowrefreshBizTrue:function()
-        {
-            var that = this;
-            that.set("showrefreshBiz",true);
-        },
-        refreshViewBiz:function()
-        {
-            app.bizAnalyzer.viewModel.setShowrefreshBizFalse();
+             showrefreshBiz.setShowrefreshBizFalse();
             $('#poorindusrsk').parent().addClass("actv");
             $('#poorindusrsk').parent().siblings().removeClass("actv");
             var tab = $('#poorindusrsk').attr("class");
             $(".tab-content").not(tab).css("display", "none");
             $(tab).show();
-            setTimeout(function() { app.bizAnalyzer.viewModel.showAfter();
-                app.bizAnalyzer.viewModel.show();
-            }, 1000);
+            setTimeout(function() { showAfter();
+                show();
+            }, 100);
               
         }
        
 
-    });
-   
-    app.bizAnalyzer = {
-       viewModel: new bizAnalyzerViewModal()	
-    };
-})(window);
+        return {
+            showrefreshBiz:showrefreshBiz,
+            showAfter: showAfter,
+            show: show,
+            getTotal: getTotal,
+            getSuccessData1: getSuccessData1,
+            refreshViewBiz: refreshViewBiz
+        };
+
+    }());
+    
+    return bizAnalyzerViewModal;
+}());

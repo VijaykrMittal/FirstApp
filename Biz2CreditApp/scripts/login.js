766e05c1c07b977be42f2660e3be5065ab42de17
Ramit Arora <ravic@biz2credit.com>
2015-02-06 07:21:39 +0000

    Changes made by Ramit Arora (ravic@biz2credit.com).

--- Biz2CreditApp/scripts/login.js
+++ Biz2CreditApp/scripts/login.js
@@ -423,6 +423,33 @@
         gsbizAnalyzerPage:function()
         {
            apps.navigate("views/getStartBizAnalyzer.html");
+        },
+        
+        slideLeft:function()
+        {
+            app.loginService.viewModel.slideAnimation("left");
+        },
+        slideRight:function()
+        {
+            app.loginService.viewModel.slideAnimation("right");
+        },
+        slideAnimation:function(options)
+        {
+            var options = {
+            "direction"        : options, // 'left|right|up|down', default 'left' (which is like 'next')
+            "duration"         :  500, // in milliseconds (ms), default 400
+            "slowdownfactor"   :    3, // overlap views (higher number is more) or no overlap (1), default 4
+            "iosdelay"         :  100, // ms to wait for the iOS webview to update before animation kicks in, default 60
+            "androiddelay"     :  150, // same as above but for Android, default 70
+            "winphonedelay"    :  250, // same as above but for Windows Phone, default 200,
+            "fixedPixelsTop"   :    0, // the number of pixels of your fixed header, default 0 (iOS only)
+            "fixedPixelsBottom":   60  // the number of pixels of your fixed footer (f.i. a tab bar), default 0 (iOS only)
+            };
+            window.plugins.nativepagetransitions.slide(
+            options,
+            function (msg) {alert("success: " + msg)}, // called when the animation has finished
+            function (msg) {alert("error: " + msg)} // called in case you pass in weird values
+            );
         }
         
     });
